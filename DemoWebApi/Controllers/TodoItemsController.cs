63f42138c80810363bba3660ca375ebeb1fd6b08
Jake Stevens <jake.stevens@improving.com>
2021-08-20 10:07:19 -0500

    init

---
+++ DemoWebApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DemoWebApi.Models;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/TodoItems")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoDbContext _context;
+
+        public TodoItemsController(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<TodoItem>>> GetToDoItems()
+        //{
+        //    return await _context.ToDoItems.ToListAsync();
+        //}
+
+        public async Task<ActionResult> GetToDoItems()
+        {
+            List<TodoItem> items = await _context.ToDoItems.ToListAsync();
+            return Json(comments);
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.ToDoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.ToDoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.ToDoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.ToDoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DemoWebApi/Models/TodoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApi.Models
+{
+	public class TodoDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+				=> options.UseSqlServer("Server=.;Database=TodoDB;Trusted_Connection=True");
+
+		//public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+		//{
+			
+		//}
+
+		public DbSet<TodoItem> ToDoItems { get; set; }
+	}
+}
